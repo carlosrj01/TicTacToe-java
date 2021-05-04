c4d3f2f2fd5198199096fcf9656ae0a674d29215
carlosrj01 <victor.capetini@gmail.com>
2021-05-03 21:17:16 -0300

    Method CheckForWin

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,13 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Game tictactoe = new Game();
-		System.out.println(tictactoe.isBoardFull());
+		
+		// Just checking if checkForWin works!
+		if (tictactoe.checkForWin()) {
+			System.out.println("Victory!");
+		} else {
+			System.out.println("NO!");
+		}
 		
 		
 	}
--- src/entities/Game.java
+++ src/entities/Game.java
@@ -8,13 +8,23 @@ public class Game {
 	public Game() {
 
 		this.board = new char[3][3];
+		currentPlayerMark = 'X'; 
+		initializeBoard();
 		drawBoard();
+	}
+
+	public char getCurrentPlayerMark() {
+		return currentPlayerMark;
+	}
+
+	public void setCurrentPlayerMark(char currentPlayerMark) {
+		this.currentPlayerMark = currentPlayerMark;
 	}
 
 	private void initializeBoard() {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				board[i][j] = '-';
+				board[i][j] = 'X'; // just testing if checkForWin works!
 			}
 		}
 
@@ -26,17 +36,15 @@ public class Game {
 		for (int i = 0; i < 3; i++) {
 			System.out.print("| ");
 			for (int j = 0; j < 3; j++) {
-				board[i][j] = 'X';
-				System.out.print(board[i][j]+" | ");
+				System.out.print(board[i][j] + " | ");
 			}
 			System.out.println("");
 			System.out.println("-------------");
 		}
 	}
-	
-	
+
 	public boolean isBoardFull() {
-		for(int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (board[i][j] == '-') {
 					return false;
@@ -46,5 +54,48 @@ public class Game {
 		return true;
 	}
 	
+	public boolean checkForWin() {
+		return checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin();
+	}
+	
 
+	private boolean checkRowsForWin() {
+		if (board[0][0] == currentPlayerMark && board[0][1] == currentPlayerMark && board[0][2] == currentPlayerMark) {
+			return true;
+		}
+		if (board[1][0] == currentPlayerMark && board[1][1] == currentPlayerMark && board[1][2] == currentPlayerMark) {
+			return true;
+		}
+		if (board[2][0] == currentPlayerMark && board[2][1] == currentPlayerMark && board[2][2] == currentPlayerMark) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean checkColumnsForWin() {
+		if (board[0][0] == currentPlayerMark && board[1][0] == currentPlayerMark && board[2][0] == currentPlayerMark) {
+			return true;
+		}
+		if (board[0][1] == currentPlayerMark && board[1][1] == currentPlayerMark && board[2][1] == currentPlayerMark) {
+			return true;
+		}
+		if (board[0][2] == currentPlayerMark && board[1][2] == currentPlayerMark && board[2][2] == currentPlayerMark) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	private boolean checkDiagonalsForWin() {
+		if (board[0][0] == currentPlayerMark && board[1][1] == currentPlayerMark && board[2][2] == currentPlayerMark) {
+			return true;
+		}
+		if (board[0][2] == currentPlayerMark && board[1][1] == currentPlayerMark && board[2][0] == currentPlayerMark) {
+			return true;
+		}
+
+		return false;
+	}
+	
 }
